4ef37eefef655ef9c695ec3b460f443708ebc968
Анастасія Савченко <savyle4ka13@gmail.com>
2022-12-28 21:10:53 +0000

    fix 3 solution

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -7,7 +7,10 @@ const refs = {
 };
 
 const INPUT_KEY = 'feedback-form-state';
-const formData = {};
+let formData = JSON.parse(localStorage.getItem(INPUT_KEY)) || {
+  email: '',
+  message: '',
+};
 
 refs.form.addEventListener('submit', onFormSubmit);
 refs.form.addEventListener('input', throttle(onFormInput, 500));
@@ -19,27 +22,29 @@ refs.form.addEventListener('input', throttle(onFormInput, 500));
 // }), 5000));
 
 function onFormInput(e) {
-    formData[e.target.name] = e.target.value;
-    localStorage.setItem(INPUT_KEY, JSON.stringify(formData));
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(INPUT_KEY, JSON.stringify(formData));
 
-    // console.log(formData);
-};
-
-function onFormSubmit(e) {
-    e.preventDefault();
-    e.currentTarget.reset();
-    localStorage.removeItem(INPUT_KEY);
-    console.log(formData);
+  // console.log(formData);
 }
 
-function fillForm (){
-    const savedValue = JSON.parse(localStorage.getItem(INPUT_KEY));
-    
-    if (savedValue){
-        refs.email.value = savedValue.email;
-        refs.message.value = savedValue.message;
-    }
-};
+function onFormSubmit(e) {
+  e.preventDefault();
+  e.currentTarget.reset();
+  localStorage.removeItem(INPUT_KEY);
+  console.log(formData);
+}
 
-fillForm ();
+function fillForm() {
+  const savedValue = formData;
 
+  if (savedValue) {
+    // formData.email = savedValue.email || '';
+    // formData.message = savedValue.message || '';
+
+    refs.email.value = formData.email;
+    refs.message.value = formData.message;
+  }
+}
+
+fillForm();
